132cdbd21b3f2652e0b9b2245f5e45f8e44d7f13
Mans Matulewicz <mans.matulewicz@gmail.com>
2017-03-13 15:54:29 +0100

    fix.sh: one foreloop and removal of md files

--- fix.sh
+++ fix.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-for f in *.md; do echo $f; reveal-md $f --static ${f%.*};  done
-for f in *.md; do echo $f; sed -e "s#href=\"/css#href=\"/presentations/${f%.*}/css#g" -i ${f%.*}/index.html; done
-for f in *.md; do echo $f; sed -e "s#src=\"/lib#src=\"/presentations/${f%.*}/lib#g" -i ${f%.*}/index.html; done
-for f in *.md; do echo $f; sed -e "s#src=\"/js#src=\"/presentations/${f%.*}/js#g" -i ${f%.*}/index.html; done
-for f in *.md; do echo $f; sed -e "s#src: '/lib#src: '/presentations/${f%.*}/lib#g" -i ${f%.*}/index.html; done
-for f in *.md; do echo $f; sed -e "s#src: '/plugin#src: '/presentations/${f%.*}/plugin#g" -i ${f%.*}/index.html; done
+
+for f in *.md
+do 
+  echo $f
+  reveal-md $f --static ${f%.*}
+  sed -e "s#href=\"/css#href=\"/presentations/${f%.*}/css#g" -i ${f%.*}/index.html
+  sed -e "s#src=\"/lib#src=\"/presentations/${f%.*}/lib#g" -i ${f%.*}/index.html
+  sed -e "s#src=\"/js#src=\"/presentations/${f%.*}/js#g" -i ${f%.*}/index.html
+  sed -e "s#src: '/lib#src: '/presentations/${f%.*}/lib#g" -i ${f%.*}/index.html
+  sed -e "s#src: '/plugin#src: '/presentations/${f%.*}/plugin#g" -i ${f%.*}/index.html
+  rm $f
+done
