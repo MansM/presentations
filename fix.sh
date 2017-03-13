2fc79b4f0b48e6c9c52808c4c94c5bb63888646e
Mans Matulewicz <mans.matulewicz@gmail.com>
2017-03-13 15:44:00 +0100

    added fix.sh

--- fix.sh
+++ fix.sh
@@ -3,5 +3,5 @@ for f in *.md; do echo $f; reveal-md $f --static ${f%.*};  done
 for f in *.md; do echo $f; sed -e "s#href=\"/css#href=\"/presentations/${f%.*}/css#g" -i ${f%.*}/index.html; done
 for f in *.md; do echo $f; sed -e "s#src=\"/lib#src=\"/presentations/${f%.*}/lib#g" -i ${f%.*}/index.html; done
 for f in *.md; do echo $f; sed -e "s#src=\"/js#src=\"/presentations/${f%.*}/js#g" -i ${f%.*}/index.html; done
-for f in *.md; do echo $f; sed -e "s#src: \'/lib#src: \'/presentations/${f%.*}/lib#g" -i ${f%.*}/index.html; done
-for f in *.md; do echo $f; sed -e "s#src: \'/plugin#src: \'/presentations/${f%.*}/plugin#g" -i ${f%.*}/index.html; done
+for f in *.md; do echo $f; sed -e "s#src: '/lib#src: '/presentations/${f%.*}/lib#g" -i ${f%.*}/index.html; done
+for f in *.md; do echo $f; sed -e "s#src: '/plugin#src: '/presentations/${f%.*}/plugin#g" -i ${f%.*}/index.html; done
