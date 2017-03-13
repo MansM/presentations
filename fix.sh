70b244705027365ed07b786abf632cf2dae554f1
Mans Matulewicz <mans.matulewicz@gmail.com>
2017-03-13 16:10:25 +0100

    creating a listing of all presentations

--- fix.sh
+++ fix.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "<html><head><title>Presentations</title></head><body>" > index.html
+echo "<h1>Presentations</h1>" >> index.html
+echo "<ul>" >> index.html
 for f in *.md
 do 
   echo $f
@@ -9,5 +12,8 @@ do
   sed -e "s#src=\"/js#src=\"/presentations/${f%.*}/js#g" -i ${f%.*}/index.html
   sed -e "s#src: '/lib#src: '/presentations/${f%.*}/lib#g" -i ${f%.*}/index.html
   sed -e "s#src: '/plugin#src: '/presentations/${f%.*}/plugin#g" -i ${f%.*}/index.html
+  echo "<li><a href=\"${f%.*}\">${f%.*}</a></li>" >> index.html
   rm $f
 done
+echo "</ul>" >> index.html
+echo "</body></html>" >> index.html
